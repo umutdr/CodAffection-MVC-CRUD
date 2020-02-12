ab9b311d544b2caa893b7ee29c78d28adfd9f635
Umut <umutdr98@gmail.com>
2020-02-12 23:46:33 +0300

    EF Core Added Initial Migration Done Employee Model Created Development Db Connection Done

---
+++ CodAffection-MVC-CRUD/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CodAffection_MVC_CRUD.Models
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> dbContextOption) : base(dbContextOption) { }
+
+        DbSet<Employee> Employees { get; set; }
+
+    }
+}
--- CodAffection-MVC-CRUD/Startup.cs
+++ CodAffection-MVC-CRUD/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodAffection_MVC_CRUD.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,8 +32,10 @@ namespace CodAffection_MVC_CRUD
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Development-MSSQL"))); // Magic string comes from appsettings.json
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
